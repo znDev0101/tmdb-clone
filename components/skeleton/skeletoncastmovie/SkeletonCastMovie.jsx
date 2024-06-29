80043bcd326af5bd5dac5ef2828db9e5512302b8
zulfa nurhuda <zulfaanurhuda28@gmail.com>
2024-06-29 15:41:14 +0700

    add skeleton loading

--- components/skeleton/skeletoncastmovie/SkeletonCastMovie.jsx
+++ components/skeleton/skeletoncastmovie/SkeletonCastMovie.jsx
@@ -5,7 +5,7 @@ const SkeletonCastMovie = () => {
     <div className="w-full flex gap-x-4 lg:px-10 lg:mb-10">
       {Array.from({ length: 8 }).map((_, i) => {
         return (
-          <div className="flex flex-col gap-y-3">
+          <div className="flex flex-col gap-y-3" key={i}>
             <div className="w-40 h-48 bg-gray-300 animate-pulse"></div>
             <div className="w-full h-5 rounded-md bg-gray-300 animate-pulse"></div>
           </div>
